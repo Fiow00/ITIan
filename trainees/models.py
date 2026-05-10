8fa14a0ac5d2f2351bcf4dc2ffaa2a1ff309d6ea
Fiow00 <ahmedyhya5325500@gmail.com>
2026-05-10 14:20:51 +0300

    Updated Trainee model
    - Added course field as a foreignkey
    - Added is_active field to implement soft deletion

--- trainees/models.py
+++ trainees/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+from courses.models import Course
+
 # Create your models here.
 class Trainee(models.Model):
     name = models.CharField(max_length=64, null=False)
     age = models.PositiveIntegerField()
     degree = models.DecimalField(decimal_places=2, max_digits=4, null=False)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.RESTRICT,
+        related_name="courses",
+    )
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
