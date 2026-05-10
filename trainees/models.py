01bd48b916d03047f1d02bce3c2dd520ea8f7140
Fiow00 <ahmedyhya5325500@gmail.com>
2026-05-10 17:06:22 +0300

    Update Trainee model
    - Added image field
    - updated course relation name

--- trainees/models.py
+++ trainees/models.py
@@ -4,13 +4,14 @@ from courses.models import Course
 
 # Create your models here.
 class Trainee(models.Model):
-    name = models.CharField(max_length=64, null=False)
+    name = models.CharField(max_length=64, null=False, blank=False)
     age = models.PositiveIntegerField()
-    degree = models.DecimalField(decimal_places=2, max_digits=4, null=False)
+    degree = models.DecimalField(decimal_places=2, max_digits=4, null=False, blank=False)
+    image = models.ImageField(upload_to="trainees", null=True, blank=True)
     course = models.ForeignKey(
         Course,
         on_delete=models.RESTRICT,
-        related_name="courses",
+        related_name="trainees",
     )
     is_active = models.BooleanField(default=True)
 
