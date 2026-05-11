bdb8a1a590e942d3e5d84672d90312e99de57bd6
Fiow00 <ahmedyhya5325500@gmail.com>
2026-05-11 15:57:08 +0300

    Implement authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-def login(request):
-    return render(request, "accounts/login.html")
+from django.contrib.auth.forms import UserCreationForm
 
-def register(request):
-    return render(request, "accounts/register.html")
+from django.views.generic.edit import CreateView
 
-def logout(request):
-    return render(request, "accounts/logout.html")
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
